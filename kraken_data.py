a3d8cd07a7bccdb326659a2a0684b7db892133d9
apariciolaya7-byte <apariciolaya7@gmail.com>
2025-12-23 00:04:08 +0000

    feat: cambio de la kill zone

--- kraken_data.py
+++ kraken_data.py
@@ -425,8 +425,8 @@ def preprocess_data_for_time_bias(df):
 
 # Tiempos de ejemplo para la superposición Londres/Nueva York:
 # La Kill Zone es de 08:00 a 12:00 UTC (4 horas de alta volatilidad)
-KILL_ZONE_START = 14
-KILL_ZONE_END = 18
+KILL_ZONE_START = 24
+KILL_ZONE_END = 00
 
 def mark_kill_zones(df):
     """
